d113950ab99df08d5f3915aa053e91893f022728
Salvador <csalvador58@gmail.com>
2022-09-18 00:47:34 -0700

    Set up wallet connect and follow feature

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -4,6 +4,9 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { client, getProfileById, getPublicationsById } from "../../api";
+import ABI from "../../abi.json";
+import { ethers } from 'ethers';
+
 
 export default function Profile() {
 
@@ -11,11 +14,9 @@ export default function Profile() {
     const [pubs, setPubs] = useState([]);
     const router = useRouter();
     const { id } = router.query;
-    // return (
-    //     <div>
-    //         ID: {id}
-    //     </div>
-    // );
+    const CONTRACT_ADDRESS = "0x60Ae865ee4C725cd04353b5AAb364553f56ceF82";
+    const [accounts, setAccounts] = useState(null);
+
 
     useEffect(() => {
         if (id) {
@@ -37,11 +38,29 @@ export default function Profile() {
         }
     }
 
-    // return (
-    //     <div>
-    //         {profile && <div>{profile.handle}</div>}
-    //     </div>
-    // )
+    async function connectWallet() {
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        });
+        console.log("accounts: ", accounts);
+        setAccounts(accounts);
+    }
+
+    async function followUser() {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+      
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+      
+        try {
+          const tx = await contract.follow([id], [0x0]);
+          await tx.wait();
+          console.log("Followed user successfully");
+        } catch (err) {
+          console.log("Failed to follow user due to", err);
+        }
+    }
+
     return (
         <Layout>
           <Head>
@@ -91,6 +110,23 @@ export default function Profile() {
                     </div>
                     <p className="mb-4">{profile.bio}</p>
                     {/* Add connect and follow buttons here */}
+                    {accounts ? (
+                        <button
+                            onClick={followUser}
+                            type="button"
+                            className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-emerald-600 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+                        >
+                            Follow {profile.handle}
+                        </button>
+                    ) : (
+                        <button
+                            onClick={connectWallet}
+                            type="button"
+                            className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-emerald-700 bg-emerald-100 hover:bg-emerald-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500"
+                        >
+                            Connect Wallet
+                        </button>
+                    )}
                   </div>
                   {/* Add publications here */}
                   {pubs.length > 0 && (
